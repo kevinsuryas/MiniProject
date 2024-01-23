4f5d1d1c9d6a58f095041e7d7e9f315b12419916
rhastaphobia <rhastaphobia@gmail.com>
2024-01-23 12:12:11 +0700

    feat: register login user and Organizer

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,68 +1,32 @@
-import express, {
-  json,
-  urlencoded,
-  Express,
-  Request,
-  Response,
-  NextFunction,
-  Router,
-} from 'express';
-import cors from 'cors';
-import { PORT } from './config';
-import { SampleRouter } from './routers/sample.router';
 
-export default class App {
-  private app: Express;
+// import express, { Express, Request, Response, NextFunction } from "express";
+// import cors from "cors";
 
-  constructor() {
-    this.app = express();
-    this.configure();
-    this.routes();
-    this.handleError();
-  }
 
-  private configure(): void {
-    this.app.use(cors());
-    this.app.use(json());
-    this.app.use(urlencoded({ extended: true }));
-  }
 
-  private handleError(): void {
-    // not found
-    this.app.use((req: Request, res: Response, next: NextFunction) => {
-      if (req.path.includes('/api/')) {
-        res.status(404).send('Not found !');
-      } else {
-        next();
-      }
-    });
+// const app: Express = express();
+// app.use(cors());
 
-    // error
-    this.app.use(
-      (err: Error, req: Request, res: Response, next: NextFunction) => {
-        if (req.path.includes('/api/')) {
-          console.error('Error : ', err.stack);
-          res.status(500).send('Error !');
-        } else {
-          next();
-        }
-      },
-    );
-  }
 
-  private routes(): void {
-    const sampleRouter = new SampleRouter();
+// const port: any = 8000;
 
-    this.app.get('/', (req: Request, res: Response) => {
-      res.send(`Hello, Purwadhika Student !`);
-    });
 
-    this.app.use('/samples', sampleRouter.getRouter());
-  }
+// import route from "./routers";
 
-  public start(): void {
-    this.app.listen(PORT, () => {
-      console.log(`  ➜  [API] Local:   http://localhost:${PORT}/`);
-    });
-  }
-}
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("<h1>Welcome to Dashboard API</h1>");
+// });
+
+// app.use(route);
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//   res.status(500).send({
+//     error: true,
+//     message: err.message || "Something Wrong!",
+//     data: null,
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`[SERVER] Server Running on Port ${port}`);
+// });
---
+++ apps/api/src/controllers/userController.ts
@@ -0,0 +1,120 @@
+// Handle Request & Response
+import { Request, Response, NextFunction } from 'express';
+import prisma from '../connection';
+import { hashPassword, hashMatch } from '../utils/hashPassword';
+import { jwtCreate } from '../utils/jwt';
+import { transporterNodemailer } from '../utils/transportMailer';
+import fs from 'fs';
+import Handlebars from 'handlebars';
+
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { email, username, password, role } = req.body;
+
+    if (!email || !username || !password || !role)
+      throw { message: 'Data Not Complete!' };
+
+    const hashedPassword: string = await hashPassword(password);
+
+    const createUser = await prisma.users.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+        role: 'USER',
+      },
+    });
+
+    const token = await jwtCreate({ id: createUser.id, role: createUser.role });
+
+    const template = fs.readFileSync('src/TemplateUser.html', 'utf-8');
+
+    let compiledTemplate: any = await Handlebars.compile(template);
+    compiledTemplate = compiledTemplate({ username, token });
+
+    await transporterNodemailer.sendMail({
+      from: 'masdefry20@gmail.com',
+      to: email,
+      subject: 'Welcome!',
+      html: compiledTemplate,
+    });
+
+    res.status(200).send({
+      error: false,
+      message: 'Register Success',
+      data: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { usernameOrEmail, password } = req.body;
+
+    const users = await prisma.users.findFirst({
+      where: {
+        OR: [{ email: usernameOrEmail }, { username: usernameOrEmail }],
+      },
+    });
+
+    if (users === null) throw { message: 'Username or Email Not Found' };
+
+    const isCompare = await hashMatch(password, users.password);
+
+    if (isCompare === false) throw { message: 'Password Doesnt Match' };
+
+    const token = await jwtCreate({ id: users.id, role: users.role });
+
+    res.status(200).send({
+      error: false,
+      message: 'Login Success',
+      data: {
+        username: users.username,
+        token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const verifiedAccount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+
+
+  const decodetoken = (req as any).payload;
+
+  try {
+    await prisma.users.update({
+      where: {
+        id: decodetoken.id,
+      },
+      data: {
+        verified: 1,
+      },
+    });
+
+
+
+    res.status(200).send({
+      error: false,
+      message: 'Verified Success',
+      data: null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
